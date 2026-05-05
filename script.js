8d077f3344084b616c54cc51431a21186284c0e5
poyodoodles <yasmin27706@gmail.com>
2026-05-05 13:45:34 +0100

    tabs working

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("collapsibleCV");
+var i;
+
+for (var i = 0; i < coll.length; ++i) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+var coll = document.getElementsByClassName("collapsiblePS");
 var i;
 
 for (var i = 0; i < coll.length; ++i) {
