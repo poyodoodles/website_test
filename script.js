e25e8f4830690746b4f6787b2482a797238195c8
poyodoodles <yasmin27706@gmail.com>
2026-05-07 23:47:49 +0100

    more content added

--- script.js
+++ script.js
@@ -28,6 +28,21 @@ for (var i = 0; i < coll.length; ++i) {
   });
 }
 
+var coll = document.getElementsByClassName("collapsibleB");
+var i;
+
+for (var i = 0; i < coll.length; ++i) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 var coll = document.getElementsByClassName("collapsibleParticles");
 var i;
 
